aeab105422c8218b7ae8a8514cd83a8cb6bc9398
dvprokofiev <d@dvprokofiev.ru>
2026-03-07 18:55:32 +0300

    feat(db): update email_verification_table migration

--- internal/database/migrations/0002_create_email_verifitaction_table.sql
+++ internal/database/migrations/0002_create_email_verifitaction_table.sql
@@ -2,10 +2,13 @@
 CREATE TABLE email_verifications (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    token UUID DEFAULT gen_random_uuid(),
+    token UUID NOT NULL DEFAULT gen_random_uuid(),
     expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT unique_user_verification UNIQUE (user_id)
 );
-
+CREATE UNIQUE INDEX idx_email_verifications_token ON email_verifications(token);
+CREATE INDEX idx_email_verifications_expires_at ON email_verifications(expires_at);
 -- +goose Down
 DROP TABLE email_verifications;
